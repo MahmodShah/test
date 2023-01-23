b6266b2923ecc51c59b72e37d80a20d347532d5c
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2023-01-23 12:13:53 +0100

    added a button

--- src/hookComponent/ComD.js
+++ src/hookComponent/ComD.js
@@ -7,6 +7,7 @@ function ComD() {
     <div>
       <h1>Count: {countContext.countC}</h1>
       <button onClick={() => countContext.dispatchC('increament')}>increament</button>
+      <button onClick={() => countContext.dispatchC('increament')}>increament</button>
       <button onClick={() => countContext.dispatchC('decreament')}>increament</button>
       <button onClick={() => countContext.dispatchC('reseat')}>increament</button>
     </div>
